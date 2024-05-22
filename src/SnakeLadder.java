cdaf796ec5ab46ebc16c6e5f211822842c6fd94c
smitalpatil98 <patilsmital.1101998gmail.com>
2024-05-22 17:42:01 +0530

    Initial commit

---
+++ src/SnakeLadder.java
@@ -0,0 +1,14 @@
+
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        int diceRoll = 0;
+        int current_position = 0;
+        int max_position = 100;
+
+        System.out.println("Welcome to Snake and Ladder Game!");
+
+        System.out.println("Start position :" + current_position);
+
+    }
+}
