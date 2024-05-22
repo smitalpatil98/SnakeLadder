e9b6056f49425f8548bf07c5074e564b27b41e6c
smitalpatil98 <patilsmital.1101998gmail.com>
2024-05-22 17:51:05 +0530

    roll_dice_number

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,14 +1,15 @@
 
 
-public class SnakeLadder {
+public class SnakeLadder{
     public static void main(String[] args) {
         int diceRoll = 0;
         int current_position = 0;
         int max_position = 100;
 
-        System.out.println("Welcome to Snake and Ladder Game!");//changes
-
-        System.out.println("Start position :" + current_position);
+        System.out.println("Welcome to Snake and Ladder Game!");
+        System.out.println("Current position :" + current_position);
+        diceRoll = (int) ((Math.random() * 6) + 1);
+        System.out.println("Dice Number :" + diceRoll);
 
     }
 }
